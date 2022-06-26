f83f2ab067a7e3662532f372a362c156b2b019a8
clementreiffers <reiffersclement@gmail.com>
2022-06-26 16:07:21 +0100

    nettoyage

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./stylesheet/index.css";
 import reportWebVitals from "./reportWebVitals";
-import Webcam from "react-webcam";
+import WebCam from "react-camera";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Webcam />
-  </React.StrictMode>
-);
+
+root.render(<WebCam />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
