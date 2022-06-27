ec891192f9ddbb62f64fd61a02daee19c0b33bcb
clementreiffers <reiffersclement@gmail.com>
2022-06-27 22:39:41 +0100

    complete video in canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
-import React from "react";
-import WebCam from "react-camera";
+import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
 
-const Canvas = (props) => {
+const App = (props) => {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+  }, [webcamRef]);
+
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user",
+  };
+
+  const draw = (image, ctx, canvas) => {
+    ctx.reset();
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.beginPath();
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    ctx.fill();
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    const context = canvas.getContext("2d");
+
+    let animationFrameId;
+
+    const render = () => {
+      draw(webcamRef.current.video, context, canvas);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    return () => window.cancelAnimationFrame(animationFrameId);
+  }, [draw]);
+
   return (
     <>
-      <div>
-        <canvas></canvas>
-        <WebCam></WebCam>
-      </div>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <canvas ref={canvasRef} />
     </>
   );
 };
 
-export default Canvas;
+export default App;
