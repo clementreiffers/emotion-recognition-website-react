c99bb3d8c2173a7e246335ddecbc15a45ba1a3b8
clementreiffers <reiffersclement@gmail.com>
2022-06-27 23:17:15 +0100

    load de blazeface ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import Webcam from "react-webcam";
+import * as blazeface from "@tensorflow-models/blazeface";
 
 const App = (props) => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  let faceRecognition;
 
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -27,11 +29,18 @@ const App = (props) => {
     const canvas = canvasRef.current;
 
     const context = canvas.getContext("2d");
+    let video = webcamRef.current.video;
 
     let animationFrameId;
+    let face;
+    faceRecognition = blazeface.load();
 
-    const render = () => {
-      draw(webcamRef.current.video, context, canvas);
+    const detectFaces = async () => {
+      const face = await faceRecognition.estimateFaces(video, false);
+    };
+
+    const render = async () => {
+      draw(video, context, canvas);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
