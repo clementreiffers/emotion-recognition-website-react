432fa889a5e28660cf27c7bda509d06db5a3df50
clementreiffers <reiffersclement@gmail.com>
2022-07-05 22:48:32 +0100

    emotions affichées dans le termial

--- src/components/WebcamModified.jsx
+++ src/components/WebcamModified.jsx
@@ -2,12 +2,74 @@ import Webcam from "react-webcam";
 import { useFaceDetection } from "react-use-face-detection";
 import FaceDetection from "@mediapipe/face_detection";
 import { Camera } from "@mediapipe/camera_utils";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as R from "ramda";
+import * as tf from "@tensorflow/tfjs";
+import "../stylesheet/WebcamModified.css";
 
 const cameraDisplayStyle = {
   width: "60%",
   position: "absolute",
+};
+
+const link =
+  "https://raw.githubusercontent.com/Im-Rises/emotion-recognition-website/main/resnet50js_ferplus/model.json";
+
+const drawOnCanvas = (context, video, boundingBox, emotionRecognizer) => {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  context.drawImage(video, 0, 0, context.canvas.width, context.canvas.height);
+
+  // recuperation of all values into boudingBox (coordinate of face)
+  const xCenterBoundingBox = R.pluck("xCenter", boundingBox);
+  const yCenterBoundingBox = R.pluck("yCenter", boundingBox);
+  const widthBoundingBox = R.pluck("width", boundingBox);
+  const heightBoundingBox = R.pluck("height", boundingBox);
+
+  // rectangle draw all around the face
+  context.beginPath();
+  context.lineWidth = "0.8";
+  context.strokeStyle = "red";
+  context.rect(
+    xCenterBoundingBox * context.canvas.width,
+    yCenterBoundingBox * context.canvas.height,
+    widthBoundingBox * context.canvas.width,
+    heightBoundingBox * context.canvas.height
+  );
+  context.stroke();
+
+  // recuperation of face only if boundingBox has valuable coordinates
+  if (
+    xCenterBoundingBox > 0 &&
+    yCenterBoundingBox > 0 &&
+    widthBoundingBox > 0 &&
+    heightBoundingBox > 0
+  ) {
+    let face = context.getImageData(
+      R.pluck("xCenter", boundingBox) * context.canvas.width,
+      R.pluck("yCenter", boundingBox) * context.canvas.height,
+      R.pluck("width", boundingBox) * context.canvas.width,
+      R.pluck("height", boundingBox) * context.canvas.height
+    );
+    if (typeof emotionRecognizer != "undefined") {
+      tf.engine().startScope();
+      tf.tidy(() => {
+        // Conversion to tensor4D and resize
+        let tfImage = tf.browser.fromPixels(face, 3).expandDims(0);
+        let tfResizedImage = tf.image.resizeBilinear(tfImage, [80, 80]);
+        tfResizedImage = tfResizedImage.reshape([1, 80, 80, 3]);
+        let prediction = Array.from(
+          emotionRecognizer.predict(tfResizedImage).dataSync()
+        );
+        // currentEmotion = getBestEmotion(prediction);
+        // results.innerHTML = magnifyResults(emotions)(prediction);
+        console.log(prediction);
+        tfImage.dispose();
+      });
+      // Check tensor memory leak stop
+      tf.engine().endScope();
+    }
+  }
 };
 
 const WebcamModified = () => {
@@ -29,48 +91,26 @@ const WebcamModified = () => {
     });
   let canvasRef = useRef(null);
 
-  const drawOnCanvas = (context, video, boundingBox) => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    context.drawImage(video, 0, 0, context.canvas.width, context.canvas.height);
-    context.beginPath();
-    context.lineWidth = "2";
-    context.strokeStyle = "red";
-    const xCenterBoundingBox = R.pluck("xCenter", boundingBox);
-    const yCenterBoundingBox = R.pluck("yCenter", boundingBox);
-    const widthBoundingBox = R.pluck("width", boundingBox);
-    const heightBoundingBox = R.pluck("height", boundingBox);
-
-    context.rect(
-      xCenterBoundingBox * context.canvas.width,
-      yCenterBoundingBox * context.canvas.height,
-      widthBoundingBox * context.canvas.width,
-      heightBoundingBox * context.canvas.height
-    );
-
-    if (
-      xCenterBoundingBox > 0 &&
-      yCenterBoundingBox > 0 &&
-      widthBoundingBox > 0 &&
-      heightBoundingBox > 0
-    ) {
-      let face = context.getImageData(
-        R.pluck("xCenter", boundingBox) * context.canvas.width,
-        R.pluck("yCenter", boundingBox) * context.canvas.height,
-        R.pluck("width", boundingBox) * context.canvas.width,
-        R.pluck("height", boundingBox) * context.canvas.height
-      );
+  const [model, setModel] = useState();
+  const loadModel = async (link) => {
+    try {
+      if (typeof model === "undefined") {
+        const fetchModel = await tf.loadLayersModel(link);
+        setModel(fetchModel);
+        console.log("load model success");
+      }
+    } catch (err) {
+      console.log(err);
     }
-
-    context.stroke();
   };
+  tf.ready().then(() => loadModel(link));
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     let animationFrameId;
-
     const render = () => {
-      drawOnCanvas(context, webcamRef.current.video, boundingBox);
+      drawOnCanvas(context, webcamRef.current.video, boundingBox, model);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
@@ -79,16 +119,31 @@ const WebcamModified = () => {
 
   return (
     <div>
-      <p>loading: {isLoading}</p>
-      <p>face detected: {detected ? "YES" : "NO"}</p>
-      <p>number of faces detected: {facesDetected}</p>
+      <div
+        style={{
+          position: "absolute",
+          color: "black",
+          // backgroundColor: "white",
+          borderBottomRightRadius: "20px",
+          padding: "2%",
+          opacity: 0.7,
+        }}
+      >
+        {/*<p>face detected: {detected ? "YES" : "NO"}</p>*/}
+        {/*<p>number of faces detected: {facesDetected}</p>*/}
+      </div>
       <div>
-        <canvas ref={canvasRef} style={cameraDisplayStyle} />
+        <canvas
+          ref={canvasRef}
+          width={1920}
+          height={1080}
+          // style={cameraDisplayStyle}
+          // style={{ display: "none" }}
+        />
         <Webcam
           ref={webcamRef}
-          forceScreenshotSourceSize
-          screenshotFormat="image/bitmap"
-          style={cameraDisplayStyle}
+          width={1920}
+          height={1080}
           style={{ display: "none" }}
         />
       </div>
