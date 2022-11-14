c8b2beed90a5a1c5986dba8dd52c670df89d9125
clementreiffers <reiffersclement@gmail.com>
2022-11-15 00:02:58 +0100

    ajout d'un moyen de lister les cams

--- src/components/SwitchCamera.jsx
+++ src/components/SwitchCamera.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Loading } from "./Loading";
 import { GithubLink } from "./GithubLink";
+import * as R from "ramda";
 
 type switchCameraProps = {
   handleClick: Function,
   isModelLoaded: boolean,
 };
 
-const switchCamera = (props: switchCameraProps) => (
-  <div className="info">
-    {!props.isModelLoaded ? <Loading /> : <GithubLink />}
-    <button onClick={props.handleClick}>switch camera</button>
-  </div>
-);
+const isVideoInput = ({ kind }) => kind === "videoinput";
 
-export default switchCamera;
+const isNotUndefined = R.has("kind");
+
+const removeUndefined = R.filter(isNotUndefined);
+
+const keepVideoDevices = (devices) =>
+  devices.map((dev) => {
+    if (isVideoInput(dev)) return dev;
+    return null;
+  });
+
+const getAllDevices = (setDevices) => {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then(R.pipe(keepVideoDevices, setDevices));
+};
+
+const SwitchCamera = (props: switchCameraProps) => {
+  const [devices, setDevices] = useState(null);
+
+  getAllDevices(setDevices);
+  console.log(devices);
+  return (
+    <div className="info">
+      {!props.isModelLoaded ? <Loading /> : <GithubLink />}
+      <button onClick={props.handleClick}>switch camera</button>
+    </div>
+  );
+};
+
+export default SwitchCamera;
